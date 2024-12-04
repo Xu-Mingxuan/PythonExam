e8acafb984f2ceee5db2c800e78fb511d4693f0a
Xu-Mingxuan <1136682836@qq.com>
2024-12-04 19:24:29 +0800

    创建

--- project/model/home.py
+++ project/model/home.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import torch
 from torchvision import models, transforms
 from PIL import Image
 from flask_cors import CORS  # 解决跨域问题
 from model import AlexNet
+
 # 初始化 Flask 应用
 app = Flask(__name__)
 CORS(app)  # 启用跨域请求支持
@@ -19,6 +20,11 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
+
+# 主页路由，渲染index.html
+@app.route('/')
+def index():
+    return render_template('home.html')
 
 # 预测路由
 @app.route('/predict', methods=['POST'])
@@ -48,5 +54,5 @@ def predict():
         return jsonify({'error': f'Error in model prediction: {e}'})
 
 # 启动 Flask 应用
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000,debug=True)
